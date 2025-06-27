d04568f003d8f86e769cc59ec9fd39fc720d88c0
Kobe <kobew5050@gmail.com>
2025-06-27 17:55:27 -0400

    fix (30-manage-tailscale): Script not running properly on system startup

--- 30-manage-tailscale.sh
+++ 30-manage-tailscale.sh
@@ -4,6 +4,9 @@
 # You can run `nmcli -g name con show --active` to see the networks you are currently connected to.
 ssid="my_home_wifi"
 
+# The tailscale daemon (I presume) makes this connection on system boot.
+tailscaleName="tailscale0"
+
 # This will make it so that any ethernet connection will be treated like the Wi-Fi network above.
 # You can run `nmcli -g name con show --active` while connected via ethernet to ensure that this variable's value is correct.
 # If you don't want ethernet connections to be treated like the Wi-Fi network above, remove this line and edit the 'if' statement below.
@@ -14,8 +17,8 @@ otherTrustedConnection="Wired connection"
 # https://networkmanager.dev/docs/api/latest/NetworkManager-dispatcher.html
 
 if [[ "$2" == "up" ]]; then
-  # Check if the ssid or other trusted connection was connected
-  if echo "$CONNECTION_ID" | grep -Eq "$ssid|$otherTrustedConnection"; then
+  # Check if the ssid, startup tailscale connection, or other trusted connection was connected
+  if echo "$CONNECTION_ID" | grep -Eq $ssid|$otherTrustedConnection|$tailscaleName"; then
     tailscale down
   else
     tailscale up
